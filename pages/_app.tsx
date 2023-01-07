696ddfa75a1a096f0508fc225523f7fd257215ba
cal <po6271933@outlook.com>
2023-01-07 21:14:40 +0800

    ignore type-check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,13 @@ import "#/styles/pretty_code.css"
 import type { AppProps } from 'next/app'
 import TopLayout from '#/components/topLayout'
 
-type ComponentWithPageLayout = AppProps & {
-  Component: AppProps["Component"] & {
-    PageLayout?: React.ComponentType
-  }
-}
+// type ComponentWithPageLayout = AppProps & {
+//   Component: AppProps["Component"] & {
+//     PageLayout?: React.ComponentType
+//   }
+// }
 
-export default function App({ Component, pageProps }: ComponentWithPageLayout) {
+export default function App({ Component, pageProps }) {
   //const getLayout = Component.PageLayout || ((page) => page)
   return (
     <>
