f024c597a20a8aada35f94814d18c87d29ecd484
cal <po6271933@outlook.com>
2024-01-11 21:23:37 +0800

    bug fix

--- components/mdx-head.tsx
+++ components/mdx-head.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import Script from "next/script";
 
-export function PageSEO ({frontmatter}) {
-  const title = frontmatter.title ?? ''
-  const description = frontmatter.description ?? ''
+export function PageSEO({ frontmatter }) {
+  const title = frontmatter.title ?? "";
+  const description = frontmatter.description ?? "";
 
   return (
     <>
@@ -14,14 +14,20 @@ export function PageSEO ({frontmatter}) {
         <meta property="og:title" content={title}></meta>
         <meta property="og:description" content={description}></meta>
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Q0ES402K4M"></Script>
-      <Script>
+
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Q0ES402K4M"
+      ></Script>
+      <Script id="google-analytics">
+        {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
         gtag('config', 'G-Q0ES402K4M');
+        `}
       </Script>
     </>
-  )
+  );
 }
