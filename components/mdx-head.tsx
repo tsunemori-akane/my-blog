bc7d0f34e2c33f0d39ec7463edca06c89d21964e
cal <po6271933@outlook.com>
2024-01-11 21:36:21 +0800

    google analytics

--- components/mdx-head.tsx
+++ components/mdx-head.tsx
@@ -1,33 +1,16 @@
 import Head from "next/head";
-import Script from "next/script";
 
 export function PageSEO({ frontmatter }) {
   const title = frontmatter.title ?? "";
   const description = frontmatter.description ?? "";
 
   return (
-    <>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta property="og:type" content="article"></meta>
-        <meta property="og:title" content={title}></meta>
-        <meta property="og:description" content={description}></meta>
-      </Head>
-
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-Q0ES402K4M"
-      ></Script>
-      <Script id="google-analytics">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-Q0ES402K4M');
-        `}
-      </Script>
-    </>
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:type" content="article"></meta>
+      <meta property="og:title" content={title}></meta>
+      <meta property="og:description" content={description}></meta>
+    </Head>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
 //import '../styles/globals.css'
 import '../styles/tailwind_global.css'
 import "#/styles/pretty_code.css"
-import type { AppProps } from 'next/app'
 import TopLayout from '#/components/topLayout'
-
+import Script from 'next/script'
 // type ComponentWithPageLayout = AppProps & {
 //   Component: AppProps["Component"] & {
 //     PageLayout?: React.ComponentType
@@ -14,6 +13,19 @@ export default function App({ Component, pageProps }) {
   const getLayout = Component.PageLayout || ((page) => page)
   return (
     <>
+     <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Q0ES402K4M"
+      ></Script>
+      <Script id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-Q0ES402K4M');
+        `}
+      </Script>
       <div className="flex min-h-screen flex-col">
         <TopLayout/>
         { getLayout(<Component {...pageProps} />) }
