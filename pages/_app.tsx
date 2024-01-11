6d255769eea194ac91ed63752eadcb9ef32da2a1
cal <po6271933@outlook.com>
2024-01-11 21:56:05 +0800

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 //import '../styles/globals.css'
-import '../styles/tailwind_global.css'
-import "#/styles/pretty_code.css"
-import TopLayout from '#/components/topLayout'
-import Script from 'next/script'
+import "../styles/tailwind_global.css";
+import "#/styles/pretty_code.css";
+import TopLayout from "#/components/topLayout";
+import Script from "next/script";
 // type ComponentWithPageLayout = AppProps & {
 //   Component: AppProps["Component"] & {
 //     PageLayout?: React.ComponentType
@@ -10,25 +10,25 @@ import Script from 'next/script'
 // }
 
 export default function App({ Component, pageProps }) {
-  const getLayout = Component.PageLayout || ((page) => page)
+  const getLayout = Component.PageLayout || ((page) => page);
   return (
     <>
-     <Script
+      <Script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-Q0ES402K4M"
+        src="https://www.googletagmanager.com/gtag/js?id=G-16SW6LLSL4"
       ></Script>
       <Script id="google-analytics">
         {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-Q0ES402K4M');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-16SW6LLSL4');
         `}
       </Script>
       <div className="flex min-h-screen flex-col">
-        <TopLayout/>
-        { getLayout(<Component {...pageProps} />) }
+        <TopLayout />
+        {getLayout(<Component {...pageProps} />)}
         {/* <>
           { Component.PageLayout ? (
             <Component.PageLayout>
@@ -40,5 +40,5 @@ export default function App({ Component, pageProps }) {
         </> */}
       </div>
     </>
-  )
+  );
 }
