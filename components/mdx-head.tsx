c35d42513217ffef7e6e559daa17c15b81d9f554
cal <po6271933@outlook.com>
2024-01-11 21:09:47 +0800

    add google analytics

--- components/mdx-head.tsx
+++ components/mdx-head.tsx
@@ -5,12 +5,22 @@ export function PageSEO ({frontmatter}) {
   const description = frontmatter.description ?? ''
 
   return (
-    <Head>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta property="og:type" content="article"></meta>
-      <meta property="og:title" content={title}></meta>
-      <meta property="og:description" content={description}></meta>
-    </Head>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:type" content="article"></meta>
+        <meta property="og:title" content={title}></meta>
+        <meta property="og:description" content={description}></meta>
+      </Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q0ES402K4M"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-Q0ES402K4M');
+      </script>
+    </>
   )
 }
